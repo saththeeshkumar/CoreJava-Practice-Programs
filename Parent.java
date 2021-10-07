5d41e9ac3b538e624607a1e7c85e5a4d4266fbe0
sathishkumar <sathish26798@gmail.com>
2021-10-07 15:12:44 +0530

    updated Parent.java

---
+++ Parent.java
@@ -0,0 +1,22 @@
+public class Parent
+{
+  int i = 100; 
+//int k = 98;
+static int j = 1000; 
+public void watchNews(){
+System.out.println("Watching PT news"); 
+}
+public void takeRest()
+{
+System.out.println("Taking rest");
+}
+public static void walk()
+{
+System.out.println("Going for a walk");  
+}
+  /*public static void main(String[] args)
+  {
+    walk();
+  }*/
+
+}
